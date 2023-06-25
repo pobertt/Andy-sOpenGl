ce792ce5030fdfdfa1ae6f07a956aa68df118e74
AndyDalaimo <adalaimo@gmail.com>
2023-06-25 12:48:10 +0100

    Added Texture Class
    Things Done:
    Application now creates a texture from file path and binds it to shader to render.

--- OpenGL/src/Shader.h
+++ OpenGL/src/Shader.h
@@ -26,14 +26,15 @@ public:
 	void Unbind() const;
 
 	// Set Uniforms
-	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+	void SetUniform1i(const std::string& name, int value);
 	void SetUniform1f(const std::string& name, float value);
+	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
 
 private:
 	ShaderProgramSource ParseShader(const std::string& filepath);
 	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
 	unsigned int CompileShader(unsigned int type, const std::string& source);
 
-	unsigned int GetUniformLocation(const std::string& name);
+	int GetUniformLocation(const std::string& name);
 
 };
