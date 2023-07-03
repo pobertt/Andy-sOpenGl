25e2abeb9083ffb5f8797c0bc35a01c0ae85c18b
AndyDalaimo <adalaimo@gmail.com>
2023-07-03 10:43:36 +0100

    Adding Test Frameworks
    Things to Do:
    Add a Test menu to access Multiple tests and destroy those tests on return button

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -20,6 +20,8 @@
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw_gl3.h"
 
+#include "tests/TestClearColor.h"
+
 
 int main(void)
 {
@@ -55,23 +57,23 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     {
-        float positions[] =
+        /*float positions[] =
         {
-            100.0f, 100.0f, 0.0f, 0.0f, // 0
-            200.0f, 100.0f, 1.0f, 0.0f, // 1
-            200.0f, 200.0f, 1.0f, 1.0f, // 2
-            100.0f, 200.0f, 0.0f, 1.0f  // 3
+            -50.0f,-50.0f, 0.0f, 0.0f, // 0
+             50.0f,-50.0f, 1.0f, 0.0f, // 1
+             50.0f, 50.0f, 1.0f, 1.0f, // 2
+            -50.0f, 50.0f, 0.0f, 1.0f  // 3
         };           
 
         unsigned int indices[] = {
             0, 1, 2,
             2, 3, 0
-        };
+        };*/
 
         GLCall(glEnable(GL_BLEND));
         GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
 
-        VertexArray va;
+        /*VertexArray va;
         VertexBuffer vb(positions, 4 * 4 * sizeof(float)); // 4 floats per vertex - made above in positions[]
 
         VertexBufferLayout layout;
@@ -82,13 +84,13 @@ int main(void)
         IndexBuffer ib(indices, 6);
 
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
 
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
 
-        Texture texture("res/textures/ZeldaWallPaper.png");
+        Texture texture("res/textures/skyBackground.png");
         texture.Bind();
         shader.SetUniform1i("u_Texture", 0);
 
@@ -96,46 +98,28 @@ int main(void)
         va.Unbind();
         vb.Unbind();
         ib.Unbind();
-        shader.Unbind();
+        shader.Unbind();*/
 
         Renderer renderer;
 
         ImGui::CreateContext();
         ImGui_ImplGlfwGL3_Init(window, true);
         ImGui::StyleColorsDark();
+        
+        test::TestClearColor test;
 
-        glm::vec3 translation(200, 200, 0);
-        float r = 0.0f;
-        float increment = 0.05f;
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
             renderer.Clear();
 
+            test.OnUpdate(0.0f);
+            test.OnRender();
+
             ImGui_ImplGlfwGL3_NewFrame();
-           
-            glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
-            glm::mat4 mvp = proj * view * model;
 
-            shader.Bind();
-            shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
-            shader.SetUniformMat4("u_MVP", mvp);
-
-            renderer.Draw(va, ib, shader);
-
-            if (r > 1.0f)
-                increment = -0.05f;
-            else if (r < 0.0f)
-                increment = 0.05f;
-
-            r += increment;
-
-            // ImGui Window
-            {
-                ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);   
-                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            }
+            test.OnImGuiRender();
 
             ImGui::Render();
             ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
