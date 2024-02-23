10fde934f578631a1b9c3fe16a19e03e8fe919dd
Robert Grimwade <79865756+pobertt@users.noreply.github.com>
2024-02-23 12:17:21 +0000

    changes

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -37,7 +37,7 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(960, 540, "your mum", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -58,48 +58,8 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
 
     {
-        /*float positions[] =
-        {
-            -50.0f,-50.0f, 0.0f, 0.0f, // 0
-             50.0f,-50.0f, 1.0f, 0.0f, // 1
-             50.0f, 50.0f, 1.0f, 1.0f, // 2
-            -50.0f, 50.0f, 0.0f, 1.0f  // 3
-        };           
-
-        unsigned int indices[] = {
-            0, 1, 2,
-            2, 3, 0
-        };*/
-
         GLCall(glEnable(GL_BLEND));
         GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
-
-        /*VertexArray va;
-        VertexBuffer vb(positions, 4 * 4 * sizeof(float)); // 4 floats per vertex - made above in positions[]
-
-        VertexBufferLayout layout;
-        layout.Push<float>(2);
-        layout.Push<float>(2);
-        va.AddBuffer(vb, layout);
-
-        IndexBuffer ib(indices, 6);
-
-        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-
-        Shader shader("res/shaders/Basic.shader");
-        shader.Bind();
-        shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-
-        Texture texture("res/textures/skyBackground.png");
-        texture.Bind();
-        shader.SetUniform1i("u_Texture", 0);
-
-
-        va.Unbind();
-        vb.Unbind();
-        ib.Unbind();
-        shader.Unbind();*/
 
         Renderer renderer;
 
