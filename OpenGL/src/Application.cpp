282a4d9be51ed95c3b1184e70aa0ad06414235fe
Robert Grimwade <79865756+pobertt@users.noreply.github.com>
2024-03-04 14:00:47 +0000

    tried doing more

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -101,7 +101,8 @@ int main(void)
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-            
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Draws whatever array buffer is currently bound */
